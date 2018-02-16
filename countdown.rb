ad5133093a29ec8ed5173cb72040a96344a87a6d
Lauren Pardee <lap5151@gmail.com>
2018-02-16 16:32:28 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
 #write your code here
-
-def countdown
-end
+def countdown(integer)
+  until integer == 0 do 
+    puts "#{integer} SECOND(S)!"
+    integer -=1
+  end
+    return "HAPPY NEW YEAR!"
+end 
