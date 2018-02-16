9a12a2e3283797ea0d8f59c5fcfbbc0a34df2036
Lauren Pardee <lap5151@gmail.com>
2018-02-16 16:34:58 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,7 @@ end
 def countdown_with_sleep(integer)
   until integer == 0 do
     puts "#{integer} SECOND(S)!"
-    sleep(1.second)
+    sleep(1)
     integer -=1
   end
     return "HAPPY NEW YEAR!"
